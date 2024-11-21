c5dbc19cfbab9178ad5dd620159337685a6550c9
Rohit <rohit19003@gmail.com>
2024-11-22 01:54:46 +0530

    added forgot password and change password

--- src/app/API/User/ChangePass/route.ts
+++ src/app/API/User/ChangePass/route.ts
@@ -1,10 +1,85 @@
 import { NextApiRequest } from 'next';
 import { NextResponse } from 'next/server';
+import bcrypt from 'bcrypt';
+
+import { Child, Guardian } from '@/app/lib/models/mongoose_models/user';
 
 
 export async function POST(req: NextApiRequest){
     try {
+        const streamdata = req.body;
+        const reader = streamdata.getReader();
+        const decoder = new TextDecoder('utf-8');
+        let chunk = "";
+        while(true){
+            const {done, value} = reader.read();
+            if(done){
+                break;
+            }
+            chunk += decoder.decode(value,{stream:true});
+        }
+        const data = JSON.parse(chunk);
+
+        const {identifier, oldpass, newpass, type} = data;
         
+        if(!identifier || !oldpass || !newpass || !type){
+            return NextResponse.json({
+                err:"provide all the necessary information"
+            },{status:400})
+        }
+
+        const hashedpassword = await bcrypt.hash(newpass, 10);
+
+        const regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+        let user;
+        if(regex.test(identifier)){
+            if(type==='guardian'){
+                user = await Guardian.findOne({email:identifier});
+            }else{
+                user = await Child.findOne({email:identifier});
+            }
+        }else{
+            if(type==='guardian'){
+                user = await Guardian.findOne({username:identifier});
+            }else{
+                user = await Child.findOne({username:identifier});
+            }
+        }
+
+        if(!user){
+            return NextResponse.json({
+                err:"No such user exists"
+            },{status:400})
+        }
+
+        const issame =await bcrypt.compare(oldpass, user.password);
+
+        if(!issame){
+            return NextResponse.json({
+                err:"invalid password "
+            },{status:400})
+        }
+
+        if(regex.test(identifier)){
+            if(type==='guardian'){
+                user = await Guardian.updateOne({email:identifier},{password:hashedpassword});
+            }else{
+                user = await Child.updateOne({email:identifier},{password:hashedpassword});
+            }
+        }else{
+            if(type==='guardian'){
+                user = await Guardian.updateOne({username:identifier},{password:hashedpassword});
+            }else{
+                user = await Child.updateOne({username:identifier},{password:hashedpassword});
+            }
+        }
+
+        return NextResponse.json({
+            success:true,
+            message:"password changed successfully"
+        },{status:200})
+
+
     } catch (error) {
         return NextResponse.json({err:"something went wrong"},{status:500})
     }
--- src/app/API/User/ForgotPass/route.ts
+++ src/app/API/User/ForgotPass/route.ts
@@ -1,11 +1,116 @@
 import { NextApiRequest } from 'next';
 import { NextResponse } from 'next/server';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 
+import { Child, Guardian } from '@/app/lib/models/mongoose_models/user';
+import handleEmailVerification from '@/app/lib/emailverification';
 
 export async function POST(req: NextApiRequest){
     try {
+        const streamdata = req.body;
+        const reader = streamdata.getReader();
+        const decoder = new TextDecoder('utf-8');
+        let chunk = "";
+        while(true){
+            const {done, value} = await reader.read();
+            if(done){
+                break;
+            }
+            chunk += decoder.decode(value, {stream: true});
+        }
+        const data = JSON.parse(chunk);
+
+        const {generate} = data;
+
+        if(!generate){
+            return NextResponse.json({
+                err:"please provide all required data"
+            },{status:400})
+        }
+
+        const secretKey = process.env.SECRET_KEY;
+        if(!secretKey){
+            throw new Error('no secretkey available');
+        }
+
+        if(generate){
+            const {email, type} = data;
+            let user ;
+
+            if(!email || !type){
+                return NextResponse.json({
+                    err:"please provide all required data"
+                },{status:400})
+            }
+
+            if(type === "guardian"){
+                user = await Guardian.findOne({email});
+            }else{
+                user = await Child.findOne({email});
+            }
+    
+            if(!user || !user.isverified){
+                return NextResponse.json({
+                    err:"No user with such user exists or user is not verified yet"
+                },{status:400})
+            }
+           
+            const token = await jwt.sign({email},secretKey,{expiresIn:'24h'});
+            
+            let text = `visit this link to verify your mail link ${token}`;
+            handleEmailVerification('verification of email', text, email, text);
+            return NextResponse.json({
+                err:"check your email to reset the password"
+            },{status:200})
+        }else{
+            const {password, type, token} = data;
+            let user ;
+
+            if(!password || !type || !token){
+                return NextResponse.json({
+                    err:"please provide all required data"
+                },{status:400})
+            }
+
+            const decodedtoken = jwt.verify(token,secretKey);
+
+            if(!decodedtoken.email){
+                return NextResponse.json({
+                    err:"invalid token"
+                },{status:400})
+            }
+
+            const hashedpass = await bcrypt.hash(password,10);
+
+            if(type === "guardian"){
+                user = await Guardian.findOne({email:decodedtoken.email});
+
+                if(!user){
+                    return NextResponse.json({
+                        err:"invalid token"
+                    },{status:400})
+                }
+
+                await Guardian.updateOne({email:decodedtoken.email},{password:hashedpass})
+            }else{
+                user = await Child.findOne({email:decodedtoken.email});
+                if(!user){
+                    return NextResponse.json({
+                        err:"invalid token"
+                    },{status:400})
+                }
+
+                await Child.updateOne({email:decodedtoken.email},{password:hashedpass})
+            }
+
+            return NextResponse.json({success:true, message:"password updated successfully"},{status:200})
+        }
         
     } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+        return NextResponse.json({err:"token expired"},{status:500})
+        }
         return NextResponse.json({err:"something went wrong"},{status:500})
     }
 }
